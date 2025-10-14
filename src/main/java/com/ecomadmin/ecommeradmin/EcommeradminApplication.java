179e9f38e56112ad4c5c6c6a61e5526b9f6f04e6
Deepak Salve <salved34@gmail.com>
2025-10-14 09:07:30 +0530

    add project repo

---
+++ src/main/java/com/ecomadmin/ecommeradmin/EcommeradminApplication.java
@@ -0,0 +1,13 @@
+package com.ecomadmin.ecommeradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommeradminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommeradminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomadmin/ecommeradmin/EcommeradminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomadmin.ecommeradmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommeradminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
